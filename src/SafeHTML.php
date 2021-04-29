befeeb2df6371f0e0e9a101e4ba52be3e30a7b24
5baddi <project@baddi.info>
2021-04-29 01:50:07 +0000

    Add copyright

--- src/SafeHTML.php
+++ src/SafeHTML.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Safe HTML
+ *
+ * @copyright   Copyright (c) 2021, BADDI Services. (https://baddi.info/)
+ * @license     http://opensource.org/licenses/MIT	MIT License
+ */
+
 namespace BADDIServices\SafeHTML;
 
 class SafeHTML
