c2bd38a262e053251f0988ac81df5fe988a87779
5baddi <project@baddi.info>
2021-04-30 12:40:23 +0000

    Init unit tests

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Safe HTML
+ *
+ * @copyright   Copyright (c) 2021, BADDI Services. (https://baddi.info)
+ * @license     http://opensource.org/licenses/MIT	MIT License
+ */
+
+namespace BADDIServices\SafeHTML\Tests;
+
+class TestCase
+{
+
+}
